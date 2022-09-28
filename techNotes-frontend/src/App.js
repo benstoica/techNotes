e621cbd478b119e7df8bb48e618d041f6c4bdfd0
Ben Stoica <bennystoica@gmail.com>
2022-09-27 21:52:30 -0400

    spacing

--- techNotes-frontend/src/App.js
+++ techNotes-frontend/src/App.js
@@ -13,11 +13,14 @@ const App = () => {
       <Route path='/' element={<Layout />}>
         <Route index element={<Public />} />
         <Route path='login' element={<Login />} />
+
         <Route path='dash' element={<DashLayout />}>
           <Route index element={<Welcome />} />
+
           <Route path='notes'>
             <Route index element={<NotesList />} />
           </Route>
+
           <Route path='users'>
             <Route index element={<UsersList />} />
           </Route>
