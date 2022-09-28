fe56021817af71fa1c73e1c5d26a082939f52b1d
Ben Stoica <bennystoica@gmail.com>
2022-09-27 21:51:21 -0400

    notes and users routes

--- techNotes-frontend/src/App.js
+++ techNotes-frontend/src/App.js
@@ -4,6 +4,8 @@ import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 const App = () => {
   return (
@@ -13,6 +15,12 @@ const App = () => {
         <Route path='login' element={<Login />} />
         <Route path='dash' element={<DashLayout />}>
           <Route index element={<Welcome />} />
+          <Route path='notes'>
+            <Route index element={<NotesList />} />
+          </Route>
+          <Route path='users'>
+            <Route index element={<UsersList />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
